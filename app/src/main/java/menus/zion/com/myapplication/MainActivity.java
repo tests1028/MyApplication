c642c563b58b44fe7396ab7ad45693622c4671f9
ZG Dev <tests1028@gmail.com>
2017-05-18 15:10:17 +0530

    First

---
+++ app/src/main/java/menus/zion/com/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package menus.zion.com.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int r = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((Button)findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(new Intent(MainActivity.this,MainActivity1.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/menus/zion/com/myapplication/MainActivity1.java
@@ -0,0 +1,35 @@
+package menus.zion.com.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    int r = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        ((Button)findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showView();
+            }
+        });
+    }
+
+    private void showView()
+    {
+
+        if(r ==0) {
+            r = 1;
+            ((ImageView) findViewById(R.id.img)).setVisibility(View.VISIBLE);
+        }else{
+            r = 0;
+            ((ImageView) findViewById(R.id.img)).setVisibility(View.GONE);
+        }
+    }
+}
