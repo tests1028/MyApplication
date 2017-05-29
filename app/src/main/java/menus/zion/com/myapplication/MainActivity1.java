3ed7d21b5d452066a3528512e1f1f4180f600d6d
ZG Dev <tests1028@gmail.com>
2017-05-29 12:49:16 +0530

    push test

--- app/src/main/java/menus/zion/com/myapplication/MainActivity1.java
+++ app/src/main/java/menus/zion/com/myapplication/MainActivity1.java
@@ -5,9 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+//Test for pull
 public class MainActivity1 extends AppCompatActivity {
-
+    //Test from dev
     int r = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
